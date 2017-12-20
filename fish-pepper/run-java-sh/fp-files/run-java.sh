427b8ed63dc4295efcce7325e54a0d0198890e11
Roland Huß <roland@ro14nd.de>
2017-12-20 12:23:36 +0100

    fix: typo

--- fish-pepper/run-java-sh/fp-files/run-java.sh
+++ fish-pepper/run-java-sh/fp-files/run-java.sh
@@ -156,7 +156,7 @@ max_memory() {
   local mem_file="/sys/fs/cgroup/memory/memory.limit_in_bytes"
   if [ -r "${mem_file}" ]; then
     local max_mem_cgroup="$(cat ${mem_file})"
-    local max_mem_meminfo_kb="$(cat /proc/meminfo | awk '/MemTotal/ {print $2}')
+    local max_mem_meminfo_kb="$(cat /proc/meminfo | awk '/MemTotal/ {print $2}')"
     local max_mem_meminfo="$(expr $max_mem_meminfo_kb \* 1024)"
     if [ ${max_mem_cgroup:-0} != -1 ] && [ ${max_mem_cgroup:-0} -lt ${max_mem_meminfo:-0} ]
     then
