9dd1685decf32181ae35be0f5a27e998f243a46e
Andrea Tarocchi <atarocch@redhat.com>
2019-02-28 15:16:13 +0100

    ENTESB-10252: removed spaces from noProxy input ENV var

--- fish-pepper/run-java-sh/fp-files/run-java.sh
+++ fish-pepper/run-java-sh/fp-files/run-java.sh
@@ -483,7 +483,7 @@ proxy_options() {
 
   local noProxy="${no_proxy:-${NO_PROXY:-}}"
   if [ -n "$noProxy" ] ; then
-    ret="$ret -Dhttp.nonProxyHosts=$(echo "|$noProxy" | sed -e 's/,[[:space:]]*/|/g' | sed -e 's/|\./|\*\./g' | cut -c 2-)"
+    ret="$ret -Dhttp.nonProxyHosts=$(echo "|$noProxy" | sed -e 's/,[[:space:]]*/|/g' | sed -e 's/[[:space:]]//g' | sed -e 's/|\./|\*\./g' | cut -c 2-)"
   fi
   echo "$ret"
 }
