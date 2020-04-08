03b673f8d80cae6e3b352d901964024a62c46455
Sebastien Dionne <survivant00@gmail.com>
2020-04-08 09:44:27 -0400

    Update run-java.sh
    fix the attach debug port for jdk >=9 based on IntelliJ documentation

--- fish-pepper/run-java-sh/fp-files/run-java.sh
+++ fish-pepper/run-java-sh/fp-files/run-java.sh
@@ -254,14 +254,18 @@ run_java_options() {
 
 debug_options() {
   if [ -n "${JAVA_ENABLE_DEBUG:-}" ] || [ -n "${JAVA_DEBUG_ENABLE:-}" ] ||  [ -n "${JAVA_DEBUG:-}" ]; then
-    local debug_port="${JAVA_DEBUG_PORT:-5005}"
+	local debug_port="${JAVA_DEBUG_PORT:-5005}"
     local suspend_mode="n"
     if [ -n "${JAVA_DEBUG_SUSPEND:-}" ]; then
       if ! echo "${JAVA_DEBUG_SUSPEND}" | grep -q -e '^\(false\|n\|no\|0\)$'; then
         suspend_mode="y"
       fi
     fi
-    echo "-agentlib:jdwp=transport=dt_socket,server=y,suspend=${suspend_mode},address=${debug_port}"
+	if [ "${JAVA_MAJOR_VERSION:-0}" -ge "9" ]; then
+		echo "-agentlib:jdwp=transport=dt_socket,server=y,suspend=${suspend_mode},address=*:${debug_port}"   
+	else
+		echo "-agentlib:jdwp=transport=dt_socket,server=y,suspend=${suspend_mode},address=${debug_port}"
+	fi
   fi
 }
 
